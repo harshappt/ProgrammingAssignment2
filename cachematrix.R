b3c6dca632b385748be33089fe8c5300d88ae2f9
Harsha Puthalapattu <Harsha.Puthalapattu@imail.org>
2015-08-21 15:28:26 -0600

    Matrix Inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function to create, retrieve and cache inverted matrices
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Function to encapsulate the Matrix data - source matrix and inverson of the source matrix
+makeCacheMatrix <- function(source.matrix = matrix()) {
+  ## Validate the source
+  if (!is.matrix(source.matrix)) {
+    stop("Function accepts only invertible matrix as input")
+  }
+  ## Initialize the inverted
+  inverted.matrix <- NULL
+  ## Set the source matrix
+  set <- function(y) {
+    source.matrix <<- y
+    inverted.matrix <<- NULL
+  }
+  ## function to retrieve the source matrix
+  get <- function() source.matrix
+  ## function to set the inverted matrix
+  setInverse <- function(inverse) inverted.matrix <<- inverse
+  ## function to retrieve the inverted matrix
+  getInverse <- function() inverted.matrix
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Function takes an invertible matrix as input x. 
+## If the inverse of the input matrix is already cached, function returns cached inverted matrix of the input x
+## If the inverse of the input matrix is not cached yet, function calculates the inverted matrix and sets it to the input x
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverted.matrix <- x$getInverse()
+  ## Check the cache
+  if(!is.null(inverted.matrix)) {
+    message("getting cached data")
+    return(inverted.matrix)
+  }
+  ## inverse of matrix is not cached yet. Create the inverse and sets it to the input x
+  source.matrix <- x$get()
+  inverted.matrix <- solve(source.matrix, ...)
+  x$setInverse(inverted.matrix)
+  inverted.matrix
 }
